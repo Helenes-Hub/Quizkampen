cbfd81990b00ae114eebbd17b845f89e0ce434e4
Hannes Hjelm <hannes.hjelm1@gmail.com>
2024-11-24 16:36:13 +0100

    Made methods for setting up buttons and fields to save time and not have to duplicate code

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -15,12 +15,12 @@ public class GamePanel extends JFrame implements ActionListener {
     private final int FINAL = 6;
     private int currentState = INITIAL;
 
-    private JTextField title = new JTextField("Quiz");
-    private JTextField question = new JTextField();
+    private JLabel title = new JLabel();
+    private JLabel question = new JLabel();
     private JTextField userNameField = new JTextField();
-    private JButton playButton = new JButton("Play");
-    private JButton quitButton = new JButton("Quit");
-    private JButton enterNameButton = new JButton("Enter");
+    private JButton playButton = new JButton();
+    private JButton quitButton = new JButton();
+    private JButton enterNameButton = new JButton();
     private JButton category1Button = new JButton();
     private JButton category2Button = new JButton();
     private JButton buttonA = new JButton();
@@ -28,6 +28,9 @@ public class GamePanel extends JFrame implements ActionListener {
     private JButton buttonC = new JButton();
     private JButton buttonD = new JButton();
     private Timer questionTimer;
+    private JProgressBar timerBar;
+    private int timeLeft;
+    int timeFromServer = 10; //den ska ta in tiden för timern från servern
 
     private ClassMaker currentCategory;
     private int currentQuestionIndex;
@@ -36,7 +39,6 @@ public class GamePanel extends JFrame implements ActionListener {
 
 
     public GamePanel() {
-
         setUpFrame();
         handleState();
     }
@@ -70,39 +72,8 @@ public class GamePanel extends JFrame implements ActionListener {
             QuestionClass currentQuestion = questions.get(currentQuestionIndex);
             JButton clickedButton = (JButton) e.getSource();
             String correctAnswer = currentQuestion.getCorrectAnswer();
-
-            if (clickedButton.getText().equals(correctAnswer)) {
-                clickedButton.setBackground(new Color(75, 181, 67));
-            } else {
-                clickedButton.setBackground(new Color(181, 67, 67));
-                getButtonWithAnswer(correctAnswer).setBackground(new Color(75, 181, 67));
-            }
-
-            buttonA.setEnabled(false);
-            buttonB.setEnabled(false);
-            buttonC.setEnabled(false);
-            buttonD.setEnabled(false);
-
-            Timer pauseTimer = new Timer(1000, new ActionListener() {
-
-                @Override
-                public void actionPerformed(ActionEvent e2) {
-                    buttonA.setBackground(new Color(211, 211, 211));
-                    buttonB.setBackground(new Color(211, 211, 211));
-                    buttonC.setBackground(new Color(211, 211, 211));
-                    buttonD.setBackground(new Color(211, 211, 211));
-
-                    buttonA.setEnabled(true);
-                    buttonB.setEnabled(true);
-                    buttonC.setEnabled(true);
-                    buttonD.setEnabled(true);
-
-                    checkAnswer(clickedButton.getText());
-                }
-            });
-
-            pauseTimer.setRepeats(false);
-            pauseTimer.start();
+            questionTimer.stop();
+            displayAnswer(clickedButton, correctAnswer);
         }
     }
 
@@ -144,94 +115,44 @@ public class GamePanel extends JFrame implements ActionListener {
     }
 
     private void mainMenuPanel() {
-        add(title);
-        title.setEditable(false);
-        title.setFocusable(false);
-        title.setHorizontalAlignment(SwingConstants.CENTER);
-        title.setBorder(null);
+        clearPanel();
+        setUpLabel(title, 150, 50, 400, 100, "Quiz");
         title.setFont(new Font("Impact", Font.BOLD, 100));
-        title.setBackground(new Color(0, 50, 76));
-        title.setForeground(new Color(211, 211, 211));
-        title.setBounds(150, 50, 400, 100);
 
-        add(playButton);
-        add(quitButton);
-        playButton.addActionListener(this);
-        quitButton.addActionListener(this);
-
-        playButton.setBounds(250, 250, 200, 100);
-        playButton.setBackground(new Color(211, 211, 211));
-        playButton.setFont(new Font("Impact", Font.BOLD, 30));
-        playButton.setFocusable(false);
-
-        quitButton.setBounds(250, 370, 200, 100);
-        quitButton.setBackground(new Color(211, 211, 211));
-        quitButton.setFont(new Font("Impact", Font.BOLD, 30));
-        quitButton.setFocusable(false);
+        setUpButton(playButton, 250, 250, 200, 100, "Play");
+        setUpButton(quitButton, 250, 370, 200, 100, "Quit");
     }
 
     private void enterUserNamePanel() {
-        getContentPane().removeAll();
-        revalidate();
-        repaint();
+        clearPanel();
 
-        JLabel userNameLabel = new JLabel("Enter your username");
-        userNameLabel.setFont(new Font("Impact", Font.BOLD, 30));
-        userNameLabel.setForeground(new Color(211, 211, 211));
-        userNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        userNameLabel.setBounds(150, 50, 400, 350);
-        add(userNameLabel);
+        JLabel userNameLabel = new JLabel();
+        setUpLabel(userNameLabel, 150, 50, 400, 350, "Enter your username");
 
-        userNameField.setBackground(new Color(211, 211, 211));
-        userNameField.setFont(new Font("Impact", Font.BOLD, 30));
-        userNameField.setHorizontalAlignment(SwingConstants.CENTER);
-        userNameField.setBorder(null);
-        userNameField.setBounds(150, 275, 400, 50);
-        userNameField.addActionListener(this);
-        add(userNameField);
-        userNameField.requestFocus();
+        setUpTextField(userNameField, 150, 275, 400, 50, "", true);
 
-        enterNameButton.setBounds(250, 350, 200, 50);
-        enterNameButton.setBackground(new Color(211, 211, 211));
-        enterNameButton.setFont(new Font("Impact", Font.BOLD, 30));
-        enterNameButton.setFocusable(false);
-        enterNameButton.addActionListener(this);
-        add(enterNameButton);
+        setUpButton(enterNameButton, 250, 350, 200, 50, "Enter");
+
     }
 
     private void showCategoriesPanel() {
         userNameField.setText("");
-        getContentPane().removeAll();
-        revalidate();
-        repaint();
+        clearPanel();
 
         add(title);
         title.setText("Select a category");
         title.setFont(new Font("Impact", Font.BOLD, 40));
 
-        category1Button.setText("Animals");
-        add(category1Button);
-        category1Button.setBounds(100, 300, 200, 100);
-        category1Button.setBackground(new Color(211, 211, 211));
-        category1Button.setFont(new Font("Impact", Font.BOLD, 30));
-        category1Button.setFocusable(false);
-        category1Button.addActionListener(this);
-
-        category2Button.setText("Science");
-        add(category2Button);
-        category2Button.setBounds(400, 300, 200, 100);
-        category2Button.setBackground(new Color(211, 211, 211));
-        category2Button.setFont(new Font("Impact", Font.BOLD, 30));
-        category2Button.setFocusable(false);
-        category2Button.addActionListener(this);
+        setUpButton(category1Button, 100, 300, 200, 100, "Animals");
+        setUpButton(category2Button, 400, 300, 200, 100, "Science");
 
     }
 
     private void startGamePanel(ClassMaker category) {
-
         questions = category.getQuestions();
         currentQuestionIndex = 0;
         score = 0;
+        timeLeft = timeFromServer;
         QuestionClass currentQuestion = questions.get(currentQuestionIndex);
         List<String> options = currentQuestion.getOptions();
 
@@ -244,67 +165,48 @@ public class GamePanel extends JFrame implements ActionListener {
         title.setText("Question " + (currentQuestionIndex + 1));
         title.setFont(new Font("Impact", Font.BOLD, 70));
 
-        add(question);
-        question.setBounds(50, 200, 600, 100);
-        question.setBorder(null);
-        question.setEditable(false);
-        question.setHorizontalAlignment(SwingConstants.CENTER);
-        question.setBackground(new Color(0, 50, 76));
-        question.setForeground(new Color(211, 211, 211));
-        question.setFont(new Font("Impact", Font.BOLD, 30));
-        question.setFocusable(false);
-        question.setText(currentQuestion.getQuestion());
+        setUpLabel(question, 50, 200, 600, 100, currentQuestion.getQuestion());
 
-        add(buttonA);
-        buttonA.setBounds(45, 350, 300, 100);
-        buttonA.setBackground(new Color(211, 211, 211));
-        buttonA.setFont(new Font("Impact", Font.BOLD, 30));
-        buttonA.setText(options.get(0));
-        buttonA.setFocusable(false);
-        buttonA.addActionListener(this);
+        setUpButton(buttonA, 45, 350, 300, 100, options.get(0));
+        setUpButton(buttonB, 355, 350, 300, 100, options.get(1));
+        setUpButton(buttonC, 45, 470, 300, 100, options.get(2));
+        setUpButton(buttonD, 355, 470, 300, 100, options.get(3));
 
-        add(buttonB);
-        buttonB.setBounds(355, 350, 300, 100);
-        buttonB.setBackground(new Color(211, 211, 211));
-        buttonB.setFont(new Font("Impact", Font.BOLD, 30));
-        buttonB.setText(options.get(1));
-        buttonB.setFocusable(false);
-        buttonB.addActionListener(this);
+        timerBar = new JProgressBar(0, timeFromServer);
+        timerBar.setValue(10);
+        timerBar.setForeground(new Color(75, 181, 67));
+        timerBar.setBackground(new Color(211, 211, 211));
+        timerBar.setBounds(150, 150, 400, 20);
+        add(timerBar);
 
-        add(buttonC);
-        buttonC.setBounds(45, 470, 300, 100);
-        buttonC.setBackground(new Color(211, 211, 211));
-        buttonC.setFont(new Font("Impact", Font.BOLD, 30));
-        buttonC.setText(options.get(2));
-        buttonC.setFocusable(false);
-        buttonC.addActionListener(this);
+        questionTimer = new Timer(1000, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                timeLeft--;
+                timerBar.setValue(timeLeft);
 
-        add(buttonD);
-        buttonD.setBounds(355, 470, 300, 100);
-        buttonD.setBackground(new Color(211, 211, 211));
-        buttonD.setFont(new Font("Impact", Font.BOLD, 30));
-        buttonD.setText(options.get(3));
-        buttonD.setFocusable(false);
-        buttonD.addActionListener(this);
+                if (timeLeft <= timeFromServer/3) {
+                    timerBar.setForeground(new Color(181, 67, 67));
+                }
+
+                if (timeLeft <= 0) {
+                    questionTimer.stop();
+                    String correctAnswer = questions.get(currentQuestionIndex).getCorrectAnswer();
+                    displayAnswer(null, correctAnswer);
+                }
+            }
+        });
+
+        questionTimer.start();
     }
 
     private void waitingForOtherPlayerPanel() {
-        getContentPane().removeAll();
-        revalidate();
-        repaint();
-
-        JLabel waitingForOtherPlayerLabel = new JLabel("Waiting for other player");
-        waitingForOtherPlayerLabel.setFont(new Font("Impact", Font.BOLD, 30));
-        waitingForOtherPlayerLabel.setForeground(new Color(211, 211, 211));
-        waitingForOtherPlayerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        waitingForOtherPlayerLabel.setBounds(150, 50, 400, 350);
-        getContentPane().add(waitingForOtherPlayerLabel);
+        clearPanel();
+        JLabel waitingForOtherPlayerLabel = new JLabel();
+        setUpLabel(waitingForOtherPlayerLabel,150, 50, 400, 350, "Waiting for other player");
     }
 
     private void roundFinishedPanel() {
-        getContentPane().removeAll();
-        revalidate();
-        repaint();
+        clearPanel();
 
         JLabel scoreLabel = new JLabel("Score this round: " + score);
         scoreLabel.setFont(new Font("Impact", Font.BOLD, 30));
@@ -316,7 +218,7 @@ public class GamePanel extends JFrame implements ActionListener {
 
     private void checkAnswer(String answer) {
         QuestionClass currentQuestion = questions.get(currentQuestionIndex);
-        if(answer.equals(currentQuestion.getCorrectAnswer())) {
+       if(answer.equals(currentQuestion.getCorrectAnswer())) {
             score++;
         }
         currentQuestionIndex++;
@@ -327,6 +229,50 @@ public class GamePanel extends JFrame implements ActionListener {
         else {
             finalScorePanel();
         }
+    }
+
+    private void displayAnswer(JButton clickedButton, String correctAnswer) {
+        if (clickedButton != null && clickedButton.getText().equals(correctAnswer)) {
+            clickedButton.setBackground(new Color(75, 181, 67));
+        }
+        else {
+            if(clickedButton != null) {
+                clickedButton.setBackground(new Color(181, 67, 67));
+            }
+            getButtonWithAnswer(correctAnswer).setBackground(new Color(75, 181, 67));
+        }
+
+        buttonA.setEnabled(false);
+        buttonB.setEnabled(false);
+        buttonC.setEnabled(false);
+        buttonD.setEnabled(false);
+
+        Timer pauseTimer = new Timer(1500, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e2) {
+                buttonA.setBackground(new Color(211, 211, 211));
+                buttonB.setBackground(new Color(211, 211, 211));
+                buttonC.setBackground(new Color(211, 211, 211));
+                buttonD.setBackground(new Color(211, 211, 211));
+
+                buttonA.setEnabled(true);
+                buttonB.setEnabled(true);
+                buttonC.setEnabled(true);
+                buttonD.setEnabled(true);
+
+                String answer;
+                if (clickedButton != null) {
+                    answer = clickedButton.getText();
+                } else {
+                    answer = "";
+                }
+                checkAnswer(answer);
+            }
+        });
+
+        pauseTimer.setRepeats(false);
+        pauseTimer.start();
     }
 
     private JButton getButtonWithAnswer(String answer) {
@@ -340,18 +286,22 @@ public class GamePanel extends JFrame implements ActionListener {
         QuestionClass currentQuestion = questions.get(currentQuestionIndex);
         List<String> options = currentQuestion.getOptions();
 
+        setDefaultButtonColor();
         title.setText("Question " + (currentQuestionIndex + 1));
         question.setText(currentQuestion.getQuestion());
         buttonA.setText(options.get(0));
         buttonB.setText(options.get(1));
         buttonC.setText(options.get(2));
         buttonD.setText(options.get(3));
+
+        timeLeft = timeFromServer;
+        timerBar.setValue(timeFromServer);
+        timerBar.setForeground(new Color(75, 181, 67));
+        questionTimer.start();
     }
 
     private void finalScorePanel() {
-        getContentPane().removeAll();
-        revalidate();
-        repaint();
+        clearPanel();
 
         add(title);
         title.setText("Final score");
@@ -359,21 +309,62 @@ public class GamePanel extends JFrame implements ActionListener {
         add(quitButton);
         quitButton.setBounds(250, 470, 200, 100);
 
-        JTextField scoreField = new JTextField();
-        add(scoreField);
-        scoreField.setEditable(false);
-        scoreField.setFocusable(false);
-        scoreField.setHorizontalAlignment(SwingConstants.CENTER);
-        scoreField.setBackground(new Color(0, 50, 76));
-        scoreField.setForeground(new Color(211, 211, 211));
-        scoreField.setFont(new Font("Impact", Font.BOLD, 30));
-        scoreField.setBorder(null);
-        scoreField.setBounds(200, 300, 300, 100);
-        scoreField.setText("Score: " + score + "/" + questions.size());
+        JLabel scoreField = new JLabel();
+        setUpLabel(scoreField, 200, 300, 300, 100, "Score: " + score + "/" + questions.size());
+
+    }
+
+    private void setDefaultButtonColor() {
+        buttonA.setBackground(new Color(211, 211, 211));
+        buttonB.setBackground(new Color(211, 211, 211));
+        buttonC.setBackground(new Color(211, 211, 211));
+        buttonD.setBackground(new Color(211, 211, 211));
+        buttonA.setEnabled(true);
+        buttonB.setEnabled(true);
+        buttonC.setEnabled(true);
+        buttonD.setEnabled(true);
+    }
+
+    private void setUpButton(JButton button, int x, int y, int width, int height, String text) {
+        add(button);
+        button.setBounds(x, y, width, height);
+        button.setBackground(new Color(211, 211, 211));
+        button.setFont(new Font("Impact", Font.BOLD, 30));
+        button.setFocusable(false);
+        button.addActionListener(this);
+        button.setText(text);
+    }
+
+    private void setUpTextField(JTextField textField, int x, int y, int width, int height, String text, boolean editable) {
+        add(textField);
+        textField.setBounds(x, y, width, height);
+        textField.setBackground(new Color(211, 211, 211));
+        textField.setFont(new Font("Impact", Font.BOLD, 30));
+        textField.setHorizontalAlignment(SwingConstants.CENTER);
+        textField.setBorder(null);
+        textField.setEditable(editable);
+        textField.setFocusable(editable);
+        textField.setText(text);
+        textField.requestFocus();
+        textField.addActionListener(this);
+    }
+
+    private void setUpLabel(JLabel label, int x, int y, int width, int height, String text) {
+        add(label);
+        label.setText(text);
+        label.setBounds(x, y, width, height);
+        label.setForeground(new Color(211, 211, 211));
+        label.setFont(new Font("Impact", Font.BOLD, 30));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    private void clearPanel() {
+        getContentPane().removeAll();
+        revalidate();
+        repaint();
     }
 
     public static void main(String[] args) {
         new GamePanel();
-
     }
 }
