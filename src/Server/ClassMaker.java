b3387554dcdb127ed3f725178350c3c3a0a4768c
Helenes-Hub <hrydros@gmail.com>
2024-11-26 20:02:27 +0100

    kortat ner vissa frågor

--- src/Server/ClassMaker.java
+++ src/Server/ClassMaker.java
@@ -23,7 +23,7 @@ public enum ClassMaker {
             new QuestionClass("Vilket djur har längst livslängd?",
                     Arrays.asList("Elefant", "Galapagossköldpadda", "Haj", "Papegoja"), "Galapagossköldpadda"),
             new QuestionClass("Vad är världens minsta fågelart?",
-                    Arrays.asList("Kolibri", "Svala", "Kanariefågel", "Sparv"), "Kolibri"),
+                    Arrays.asList("Kolibri", "Minibri", "Kanariefågel", "Sparv"), "Kolibri"),
             new QuestionClass("Vilket djur sover stående?",
                     Arrays.asList("Häst", "Ko", "Giraff", "Alla ovanstående"), "Alla ovanstående")
     )),
@@ -34,20 +34,20 @@ public enum ClassMaker {
                     Arrays.asList("H2O", "O2", "CO2", "Vattikus"), "H2O"),
             new QuestionClass("Viket kön börjar alla bebisar som i livmodern?",
                     Arrays.asList("Pojke", "Flicka", "Falskt påstående", "Hen"), "Flicka"),
-            new QuestionClass("Vilket organ i kroppen producerar insulin?",
+            new QuestionClass("Vilket organ producerar insulin?",
                     Arrays.asList("Levern", "Bukspottkörteln", "Magsäcken", "Njuren"), "Bukspottkörteln"),
             new QuestionClass("Vad mäter en seismograf?",
-                              Arrays.asList("Temperatur", "Lufttryck", "Jordskalv", "Vindstyrka"), "Jordskalv"),
-            new QuestionClass("Hur lång tid tar det för ljus att färdas från solen till jorden?",
-                              Arrays.asList("8 minuter", "1 minut", "30 sekunder", "10 minuter"), "8 minuter"),
-            new QuestionClass("Vilken art av djur är känd för att kunna ändra färg för att kamouflera sig?",
-                              Arrays.asList("Bläckfisk", "Kameleont", "Gecko", "Grodor"), "Kameleont"),
-            new QuestionClass("Vilken planet har den högsta temperaturen i vårt solsystem?",
+                              Arrays.asList("Grafer", "Lufttryck", "Jordskalv", "Vindstyrka"), "Jordskalv"),
+            new QuestionClass("Hur lång tid tar ljus på sig att färdas från solen till oss?",
+                              Arrays.asList("8 minuter", "1 minut", "30 sekunder", "1 sekund"), "8 minuter"),
+            new QuestionClass("Vilken djur är känd för att kunna ändra färg?",
+                              Arrays.asList("skameleont", "Kameleont", "Gecko", "Grodor"), "Kameleont"),
+            new QuestionClass("Vilken planet är den varmaste i vårt solsystem?",
                               Arrays.asList("Merkurius", "Venus", "Mars", "Jorden"), "Venus"),
             new QuestionClass("Vad är huvudkomponenten i solens energi?",
                               Arrays.asList("Fusionsreaktioner", "Kärnklyvning", "Kemiska reaktioner", "Gravitationskraft"), "Fusionsreaktioner"),
             new QuestionClass("Vad är det latinska namnet för överarmsben?",
-                              Arrays.asList("Femur", "Humerus", "Tibia", "Radius"), "Humerus")
+                              Arrays.asList("Femur", "Humerus", "armus överus", "Radius"), "Humerus")
     ));
 
     private final List<QuestionClass> questions;
